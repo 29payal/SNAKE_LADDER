4bddebfbffef0760677b7b3658dde2c203670f74
Payal Sharma <https://github.com/29payal>
2022-09-04 20:59:57 +0530

    [Payal] UC1-SinglePlayerPosition

--- SNAKEandLADDER/Program.cs
+++ SNAKEandLADDER/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to SNAKE & LADDER!");
+            Console.WriteLine("Choose option\n 1. Single player position");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    SinglePlayerPosition.position();
+                    break;
+                    default:
+                    Console.WriteLine("Wrong choice");
+                    break;
+            }
         }
     }
 }
