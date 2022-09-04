5b2a545670901ab48d3387ac2cc9687578522645
Payal Sharma <https://github.com/29payal>
2022-09-04 21:23:21 +0530

    [Payal] UC3-PlayerCheckForOption

--- SNAKEandLADDER/Program.cs
+++ SNAKEandLADDER/Program.cs
@@ -5,16 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to SNAKE & LADDER!");
-            Console.WriteLine("Choose option\n 1. Single player position\t 2.Rolls the Die");
+            Console.WriteLine("Choose option\n 1. Single player position\t 2.Rolls the Die\t 3.Player Check a for option");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
                 case 1:
                     SinglePlayerPosition.position();
                     break;
-                    case 2:
+                case 2:
                     RollTHEDies.RollDie();
                         break;
+                case 3:
+                    PlayerCheckForOption.checkoption();
+                    break;
                     default:
                     Console.WriteLine("Wrong choice");
                     break;
