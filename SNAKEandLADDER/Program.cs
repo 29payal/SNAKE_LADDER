a449b9ca99a68db32893527eb97f7da3b1944800
Payal Sharma <https://github.com/29payal>
2022-09-05 03:24:21 +0530

    [Payal] UC4-RepeatPositionTill

--- SNAKEandLADDER/Program.cs
+++ SNAKEandLADDER/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to SNAKE & LADDER!");
-            Console.WriteLine("Choose option\n 1. Single player position\t 2.Rolls the Die\t 3.Player Check a for option");
+            Console.WriteLine("Choose option\n 1. Single player position\t 2.Rolls the Die\t 3.Player Check a for option\t 4.Repeat position till 100th");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -18,6 +18,9 @@
                 case 3:
                     PlayerCheckForOption.checkoption();
                     break;
+                case 4:
+                    RepeatPostionTill100.CheckPostionTill100();
+                        break;         
                     default:
                     Console.WriteLine("Wrong choice");
                     break;
