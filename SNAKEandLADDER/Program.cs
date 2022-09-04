6b461b2c8dc9e370bc97abf07635284efd427ae4
Payal Sharma <https://github.com/29payal>
2022-09-05 03:44:10 +0530

    [Payal] UC5-WinningPosition

--- SNAKEandLADDER/Program.cs
+++ SNAKEandLADDER/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to SNAKE & LADDER!");
-            Console.WriteLine("Choose option\n 1. Single player position\t 2.Rolls the Die\t 3.Player Check a for option\t 4.Repeat position till 100th");
+            Console.WriteLine("Choose option\n 1. Single player position\t 2.Rolls the Die\t 3.Player Check a for option\t 4.Repeat position till 100th\t 5. Exact winning position till 100th");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -19,8 +19,11 @@
                     PlayerCheckForOption.checkoption();
                     break;
                 case 4:
-                    RepeatPostionTill100.CheckPostionTill100();
-                        break;         
+                    RepeatPositionTill100.CheckPostionTill100();
+                        break;
+                case 5:
+                    WinningPosition.ExactWinTill100();
+                    break;
                     default:
                     Console.WriteLine("Wrong choice");
                     break;
