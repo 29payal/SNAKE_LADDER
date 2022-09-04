816aac5fa5a78f7ca3b161e9fabfc413fbefcbff
Payal Sharma <https://github.com/29payal>
2022-09-04 21:12:17 +0530

    [Payal] UC2-RollTHEDies

--- SNAKEandLADDER/Program.cs
+++ SNAKEandLADDER/Program.cs
@@ -5,13 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to SNAKE & LADDER!");
-            Console.WriteLine("Choose option\n 1. Single player position");
+            Console.WriteLine("Choose option\n 1. Single player position\t 2.Rolls the Die");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
                 case 1:
                     SinglePlayerPosition.position();
                     break;
+                    case 2:
+                    RollTHEDies.RollDie();
+                        break;
                     default:
                     Console.WriteLine("Wrong choice");
                     break;
---
+++ SNAKEandLADDER/RollTHEDies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNAKEandLADDER
+{
+    internal class RollTHEDies
+    {
+        public static void RollDie()
+        {
+            int player1 = 0;
+            Random random = new Random();
+            player1 = random.Next(1, 6);
+            Console.WriteLine("get the number: " +player1);
+        }
+    }
+}
