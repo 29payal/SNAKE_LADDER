62445f06269815161f7c63b9c40f2d98a13706fe
Payal Sharma <https://github.com/29payal>
2022-09-05 04:01:35 +0530

    [Payal] UC6-DicePlayed

--- SNAKEandLADDER/Program.cs
+++ SNAKEandLADDER/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to SNAKE & LADDER!");
-            Console.WriteLine("Choose option\n 1. Single player position\t 2.Rolls the Die\t 3.Player Check a for option\t 4.Repeat position till 100th\t 5. Exact winning position till 100th");
+            Console.WriteLine("Choose option\n 1. Single player position\t 2.Rolls the Die\t 3.Player Check a for option\t 4.Repeat position till 100th\t 5. Exact winning position till 100th\t 6.Dice was played");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -23,6 +23,9 @@
                         break;
                 case 5:
                     WinningPosition.ExactWinTill100();
+                    break;
+                case 6:
+                    DicePlayed.DiceRoll();
                     break;
                     default:
                     Console.WriteLine("Wrong choice");
